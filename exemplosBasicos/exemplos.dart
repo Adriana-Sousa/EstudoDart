a8c15a0065f7a42850173663bd53938742f84153
Adriana Sousa <120699927+Adriana-Sousa@users.noreply.github.com>
2024-09-04 20:50:50 -0300

    Create exemplos.dart

---
+++ exemplosBasicos/exemplos.dart
@@ -0,0 +1,18 @@
+void main(){
+  var nome = "Bob"; //tipo inferido como string, naãopode mudar o tipo
+  Object name = "Bem"; //object é a classe geral dos tipos, pode mudar o tipo
+  late String firstname; //inicialização tardia 
+  const int number = 7; //não pode alterar o valor
+  final double pi; //só pode ser atribuido uma unica vez
+  
+ name = 5; //mudando de string pra int
+  print(name);
+  
+  //anulidade
+  String? palavra; //pode ser null ou string
+  String cidade; //não pode se null e precisa ser iniciada antes de usar
+  
+  print(palavra); // valor padrão como null
+  //print(cidade); não pode ser usada pois não tem valor padrão
+  
+}
